5fddedce6944ba35a2c34835b4627877a62e9b51
ywk1000 <22372766+ywk1000@users.noreply.github.com>
2022-05-23 23:20:19 +0800

    Create ql.js

---
+++ ql.js
@@ -0,0 +1,206 @@
+'use strict';
+
+const got = require('got');
+require('dotenv').config();
+const { readFile } = require('fs/promises');
+const path = require('path');
+
+const qlDir = process.env.QL_DIR || '/ql/data';
+const authFile = path.join(qlDir, 'config/auth.json');
+
+const api = got.extend({
+  prefixUrl: process.env.QL_URL || 'http://localhost:5600',
+  retry: { limit: 0 },
+});
+
+async function getToken() {
+  if (process.env.NINJA_ALONE) {
+    const authConfig = await api({
+      url: process.env.QL_URL+'/open/auth/token?client_id='+process.env.QL_CLIENT_ID+'&client_secret='+process.env.QL_CLIENT_SECRET,
+      headers: {
+        Accept: 'application/json',
+      },
+    }).json();
+    return authConfig.token;
+  } else {
+    const authConfig = JSON.parse(await readFile(authFile));
+    return authConfig.token;
+  }
+}
+
+module.exports.getEnvs = async () => {
+  const token = await getToken();
+  const body = await api({
+    url: process.env.NINJA_ALONE?'open/envs':'api/envs',
+    searchParams: {
+      searchValue: 'JD_COOKIE',
+      t: Date.now(),
+    },
+    headers: {
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+    },
+  }).json();
+  return body.data;
+};
+
+module.exports.getEnvsCount = async () => {
+  const data = await this.getEnvs();
+  return data.length;
+};
+
+module.exports.addEnv = async (cookie, remarks) => {
+  const token = await getToken();
+  const body = await api({
+    method: 'post',
+    url: process.env.NINJA_ALONE?'open/envs':'api/envs',
+    params: { t: Date.now() },
+    json:   [{
+      name: 'JD_COOKIE',
+      value: cookie,
+      remarks,
+    }],
+    headers: {
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+  }).json();
+  return body;
+};
+
+module.exports.updateEnv = async (cookie, eid, remarks) => {
+  const token = await getToken();
+  const body = await api({
+    method: 'put',
+    url: process.env.NINJA_ALONE?'open/envs':'api/envs',
+    params: { t: Date.now() },
+    json: {
+      name: 'JD_COOKIE',
+      value: cookie,
+      _id: eid,
+      remarks,
+    },
+    headers: {
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+  }).json();
+  return body;
+};
+
+module.exports.enableEnv = async (eid) => {
+
+  const token = await getToken();
+  const body = await api({
+    method: 'put',
+    url: process.env.NINJA_ALONE?'open/envs':'api/envs/enable',
+    params: { t: Date.now() },
+    json: [eid,],
+    headers: {
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+  }).json();
+  return body;
+};
+
+module.exports.delEnv = async (eid) => {
+  const token = await getToken();
+  const body = await api({
+    method: 'delete',
+    url: process.env.NINJA_ALONE?'open/envs':'api/envs',
+    params: { t: Date.now() },
+    body: JSON.stringify([eid]),
+    headers: {
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+  }).json();
+  return body;
+};
+
+//////////////////////////////////////////////////
+// wskey
+module.exports.getWSCKEnvs = async () => {
+  const token = await getToken();
+  const body = await api({
+    url: process.env.NINJA_ALONE?'open/envs':'api/envs',
+    searchParams: {
+      searchValue: 'JD_WSCK',
+      t: Date.now(),
+    },
+    headers: {
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+    },
+  }).json();
+  return body.data;
+};
+
+module.exports.getWSCKEnvsCount = async () => {
+  const data = await this.getWSCKEnvs();
+  return data.length;
+};
+
+module.exports.addWSCKEnv = async (jdwsck, remarks) => {
+  const token = await getToken();
+  const body = await api({
+    method: 'post',
+    url: process.env.NINJA_ALONE?'open/envs':'api/envs',
+    params: { t: Date.now() },
+    json: [{
+      name: 'JD_WSCK',
+      value: jdwsck,
+      remarks,
+    }],
+    headers: {
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+  }).json();
+  return body;
+};
+
+module.exports.updateWSCKEnv = async (jdwsck, eid, remarks) => {
+  const token = await getToken();
+  const body = await api({
+    method: 'put',
+    url: process.env.NINJA_ALONE?'open/envs':'api/envs',
+    params: { t: Date.now() },
+    json: {
+      name: 'JD_WSCK',
+      value: jdwsck,
+      _id: eid,
+      remarks,
+    },
+    headers: {
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+  }).json();
+  return body;
+};
+
+module.exports.delWSCKEnv = async (eid) => {
+  const token = await getToken();
+  const body = await api({
+    method: 'delete',
+    url: process.env.NINJA_ALONE?'open/envs':'api/envs',
+    params: { t: Date.now() },
+    body: JSON.stringify([eid]),
+    headers: {
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+  }).json();
+  return body;
+};
+
+//////////////////////////////////////////////////
